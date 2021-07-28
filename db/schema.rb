05670b54df9e43dec4186d2abb6c3856f96a8b02
kenya ohara <bureibu20@gmail.com>
2021-07-28 17:15:45 +0900

    feed機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_073058) do
+ActiveRecord::Schema.define(version: 2021_07_28_081129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
