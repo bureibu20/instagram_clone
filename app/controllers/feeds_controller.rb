bf6f374d10a7c9a5e903bd6bed96e98000da010a
kenya ohara <bureibu20@gmail.com>
2021-08-03 23:01:26 +0900

    private

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -70,13 +70,12 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_feed
-      @feed = Feed.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def feed_params
-      params.require(:feed).permit(:content, :image, :image_cache, :user_id)
-    end
+  def set_feed
+    @feed = Feed.find(params[:id])
+  end
+  
+  def feed_params
+    params.require(:feed).permit(:content, :image, :image_cache, :user_id)
+  end  
+    
 end
