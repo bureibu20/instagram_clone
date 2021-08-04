bba731c586c58f80acdbe891fdaaeb13f8d92287
kenya ohara <bureibu20@gmail.com>
2021-08-04 11:54:36 +0900

    画像のバリデーション

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,6 @@
 class Feed < ApplicationRecord
   mount_uploader :image, ImageUploader
-
+  validates :image, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
