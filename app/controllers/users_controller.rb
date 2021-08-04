9d45cb8ffbb93e390020d1d59ca2f29dfda7d6de
kenya ohara <bureibu20@gmail.com>
2021-08-04 23:21:04 +0900

    プロフィール修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "プロフィールを編集しました！"
+    else
+      render :edit
+    end
+  end
   def show
     @user = User.find(params[:id])
     @feeds = @user.feeds
